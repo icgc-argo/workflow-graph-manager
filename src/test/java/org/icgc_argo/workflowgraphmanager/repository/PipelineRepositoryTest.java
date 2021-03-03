727baad34f01c6a9564761ef510217481c2f9ea7
Alex Lepsa <lepsalex@gmail.com>
2021-03-03 14:38:04 -0500

    basic first test in place

--- src/test/java/org/icgc_argo/workflowgraphmanager/repository/PipelineRepositoryTest.java
+++ src/test/java/org/icgc_argo/workflowgraphmanager/repository/PipelineRepositoryTest.java
@@ -18,6 +18,7 @@
 
 package org.icgc_argo.workflowgraphmanager.repository;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.icgc_argo.workflowgraphmanager.utils.JacksonUtils.readValue;
 
 import io.fabric8.kubernetes.api.model.Pod;
@@ -25,8 +26,11 @@ import io.fabric8.kubernetes.client.KubernetesClient;
 import io.fabric8.kubernetes.client.server.mock.EnableKubernetesMockClient;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Stream;
+
 import lombok.val;
 import org.icgc_argo.workflow_graph_lib.utils.JacksonUtils;
+import org.junit.Before;
 import org.junit.jupiter.api.Test;
 import org.springframework.test.context.ActiveProfiles;
 
@@ -46,12 +50,25 @@ public class PipelineRepositoryTest {
   public void singleNodePipeline() {
     val simplePipelineJson =
         readValue(this.getClass().getResourceAsStream("fixtures/single-pipeline.json"), Map.class);
-    ((List<Map<String, Object>>) simplePipelineJson.get("items"))
-        .stream()
-            .map(podJson -> JacksonUtils.convertValue(podJson, Pod.class))
-            .forEach(pod -> client.pods().create(pod));
+
+    loadK8sWithDecoysAnd(
+        ((List<Map<String, Object>>) simplePipelineJson.get("items"))
+            .stream().map(podJson -> JacksonUtils.convertValue(podJson, Pod.class)));
 
     val result = pipelineRepository.getPipelines();
-    result.get("example");
+
+    assertThat(result.keySet().size()).isEqualTo(1);
+    assertThat(result.keySet().contains("test-pipeline")).isTrue();
+    assertThat(result.get("test-pipeline").getNodes().size()).isEqualTo(2);
+  }
+
+  private void loadK8sWithDecoysAnd(Stream<Pod> pods) {
+    val decoyPodsJson =
+        readValue(this.getClass().getResourceAsStream("fixtures/decoy-pods.json"), Map.class);
+
+    // Merge pods with decoy stream and load mock K8s
+    Stream.concat(pods, ((List<Map<String, Object>>) decoyPodsJson.get("items")).stream())
+        .map(podJson -> JacksonUtils.convertValue(podJson, Pod.class))
+        .forEach(pod -> client.pods().create(pod));
   }
 }
